2c1b67f4b12b078c5fbf839e2fc944f9988bbc18
Parrag Mehta <parrag.m.mehta2@snapon.com>
2022-02-20 12:39:45 -0800

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿namespace Service1.Controllers
+{
+    using System;
+    //using System.Net.Http;
+    //using System.Data.SqlClient;
+    using Microsoft.AspNetCore.Mvc;
+    using Service1.Models;
+
+    [Route("api/service1")]
+    public class HomeController : ControllerBase
+    {
+        //private readonly HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult GetServiceName()
+        {
+            Console.WriteLine("GetServiceName called on Service1");
+
+            return Ok(
+                new ServiceModel
+                {
+                    Id = 1,
+                    Name = "Service1"
+                }
+            );
+        }
+
+        //[HttpGet]
+        //public IActionResult GetServiceDetails()
+        //{
+        //    Console.WriteLine("GetServiceDetails called on Service1");
+
+        //    var connStr = "";
+
+        //    using (var conn = new SqlConnection(connStr))
+        //    {
+        //        conn.Open();
+                
+        //    }
+
+        //    return Ok(
+        //        new ServiceModel
+        //        {
+        //            Id = 1,
+        //            Name = "Service1"
+        //        }
+        //    );
+        //}
+
+        //[Route("call")]
+        //[HttpPost]
+        //public IActionResult CallExternalService()
+        //{
+        //    Console.WriteLine("Calling Service2");
+
+        //    try
+        //    {
+        //        var response = client.GetAsync("http://service2-clusterip:80/api/service2").Result;
+
+        //        response.EnsureSuccessStatusCode();
+        //        Console.WriteLine(response);
+
+        //        return Ok(response.StatusCode);
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        return StatusCode(500, ex.Message);
+        //    }
+        //}
+    }
+}
---
+++ Models/ServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Service1.Models
+{
+    public class ServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
